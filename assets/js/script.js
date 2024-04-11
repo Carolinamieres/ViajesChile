32d2a5ca718c367065e6b25d18ee5d68a966df5a
Carolina Mieres <cmieresm@gmail.com>
2024-04-11 19:20:17 -0400

    Septimo commit se modifica script.js

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $(".nav-link a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+
+
+
+
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
